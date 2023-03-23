039974cd39eb4ae823d438d7d448b5dc6ce9a204
Rida Khan <rida.khan2@ucalgary.ca>
2023-03-23 15:40:37 -0600

    added bucket code

--- infra/main.tf
+++ infra/main.tf
@@ -14,5 +14,14 @@ provider "aws" {
 }
 
 
+# S3 bucket
+# if you omit the name, Terraform will assign a random name to it
+# see the docs: https://registry.terraform.io/providers/hashicorp/aws/latest/docs/resources/s3_bucket
+resource "aws_s3_bucket" "lambda" {}
+
+# output the name of the bucket after creation
+output "bucket_name" {
+  value = aws_s3_bucket.lambda.bucket
+}
 
 # add the next part of the infrasturcture code from the notes once the functions are made 
