4a01a3c93971091bc783fd26872ed20b896013c0
Raisa Rafi <rafi.raisaar@gmail.com>
2023-03-24 21:07:12 -0600

    adding functions

--- functions/delete-note/main.py
+++ functions/delete-note/main.py
@@ -1,1 +1,13 @@
-# add your delete-note function here
+import boto3
+
+def handler(event, context):
+    dynamodb = boto3.resource('dynamodb')
+    table_name = "lotion-30140980"
+    table = dynamodb.Table(table_name) 
+    response = table.delete_item(
+        Key={
+            'email': 'partition_key_value',
+            'note_id': 'sort_key_value'
+        }
+    )
+    print(response)
--- functions/get-notes/main.py
+++ functions/get-notes/main.py
@@ -1,1 +1,32 @@
-# add your get-notes function here
+import boto3
+import json
+
+# create a DynamoDB resource
+dynamodb = boto3.resource("dynamodb")
+table_name = "lotion-30140980"
+table = dynamodb.Table(table_name)
+
+def handler(event, context):
+    email = event["queryStringParameters"]["email"]
+    access_token = event["headers"]["access_token"]
+
+    # TODO: Add authentication and authorization code using access_token
+
+    # retrieve all the notes for the user
+    response = table.query(
+        IndexName="email-index",
+        KeyConditionExpression="email = :email",
+        ExpressionAttributeValues={":email": email}
+    )
+
+    # format the notes into a list
+    notes = []
+    for item in response["Items"]:
+        notes.append(item["note"])
+
+    return {
+        "statusCode": 200,
+        "body": json.dumps({
+            "notes": notes
+        })
+    }
